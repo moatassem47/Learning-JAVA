6b0a8da5298540a44e80a56286c3ca058dd2dd3f
moatassem47 <moatassem.320240182@ejust.edu.eg>
2025-07-24 12:22:28 +0300

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main (String[] args){
+    // int AnInteger = 10;
+    // long Longint = 50516516518949874l;
+    // char character = 'a';
+    //      System.out.println(AnInteger);
+    //      System.out.println(Longint);
+    //      System.out.println(character);
+    
+    int number1= 1;
+    double number2 = (double)number1;
+    System.out.println(number2);
+
+    double number3 = 3;
+    System.out.println(number3);
+    int number4 = (int)number3;
+    System.out.println(number4);
+    
+    
+    char character = 'A';
+    int CharacterNumber = (int)character;
+    System.out.println(CharacterNumber);
+    
+
+}
+
+}

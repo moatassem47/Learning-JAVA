22086f63dcd8af6b68d3eda12efb2a4dd2b12fd9
moatassem47 <moatassem.320240182@ejust.edu.eg>
2025-07-26 09:01:07 +0300

    strings and string methods

---
+++ strings.java
@@ -0,0 +1,50 @@
+public class strings {
+    public static void main(String[] args) {
+        // String name = "jack";
+        // System.out.println(name);
+        // String NewName = new String("alfred");
+        // System.out.println(NewName);
+
+        // String literalString1 = "man";
+        // String literlString2 = "man";
+        
+        // String objectstring1 = new String("woman");
+        // String objectstring2 = new String("woman");
+        
+        // System.out.println(literalString1 == literlString2);
+       
+        // System.out.println(objectstring1 == objectstring2);
+
+        // String Name = "bruce wayne";
+        // String country = "USA";
+        // int age = 30;
+        // // System.out.println("hello my name is " + Name + " I am from " + country + " I am " + age +" years old");
+
+        // System.out.println(String.format("my name is %s I am from %s and I am %d years old",Name,country,age));
+
+        // String Name = "ABC";
+        // String Name2 = "abc";
+        // System.out.println(Name.isEmpty());
+        // System.out.println(Name.length());
+        // System.out.println(Name.toUpperCase());
+        // System.out.println(Name.equals(Name2));
+
+        String sky = "the sky is blue";
+        sky= sky.replace("sky", "sea");   
+        System.out.println(sky.contains("sky"));
+        
+
+
+
+
+
+
+
+
+
+
+    
+    
+    }
+
+}

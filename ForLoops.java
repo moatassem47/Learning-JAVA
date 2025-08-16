c223912171bbf8126f3a6d56dca58e960749a083
moatassem47 <moatassem.320240182@ejust.edu.eg>
2025-08-16 04:34:30 +0000

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,23 @@
+
+public class ForLoops {
+
+    public static void main(String[] args) {
+    //   int numbers[]={1,2,3,4,5,6,7,8,9,10};
+    //   int ArraySize = numbers.length-1;
+    //   int sum=0;
+    //   for(int i =0; i<=ArraySize;i++){
+    //     sum+=numbers[i];
+    // }
+    //  System.out.println(sum);
+    
+ int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+  for(int i : numbers){
+      System.out.println(i);
+  }
+    
+      
+    
+    
+    
+    }
+}
